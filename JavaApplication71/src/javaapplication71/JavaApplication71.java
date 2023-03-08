ae835fef29951492c980a1febf216df7d51027ae
Murad_Valiyev <murad.veliyev.2001@mail.ru>
2023-03-09 01:49:31 +0400

    "Tortoise"

--- JavaApplication71/src/javaapplication71/JavaApplication71.java
+++ JavaApplication71/src/javaapplication71/JavaApplication71.java
@@ -15,7 +15,7 @@ public class JavaApplication71 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("asus a hp dan salam");
+        System.out.println("tortoise");
     }
     
 }
