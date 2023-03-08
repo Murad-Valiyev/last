eb9c7daea9fb6b48f3d25fc49742067c28b078ff
The-V-Murad <muradveliyev007@gmail.com>
2023-03-09 01:35:54 +0400

    Hp atvetka

--- JavaApplication71/src/javaapplication71/JavaApplication71.java
+++ JavaApplication71/src/javaapplication71/JavaApplication71.java
@@ -15,7 +15,7 @@ public class JavaApplication71 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Asus deyisdi");
+        System.out.println("Hp canavar deyisdi");
     }
     
 }
