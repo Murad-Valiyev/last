b36ef14cb19422765f050e9374dab589fdf25f60
The-V-Murad <muradveliyev007@gmail.com>
2023-03-09 01:46:56 +0400

    Asus a hp dan salam

--- JavaApplication71/src/javaapplication71/JavaApplication71.java
+++ JavaApplication71/src/javaapplication71/JavaApplication71.java
@@ -15,7 +15,7 @@ public class JavaApplication71 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hp canavar deyisdi");
+        System.out.println("asus a hp dan salam");
     }
     
 }
